da4e697cddc13509386e79659ca0d124861fd9e6
Firstname Lastname <MY_NAME@example.com>
2023-06-20 07:29:59 -0400

    123

--- 3.Windows11_Download_KACE.ps1
+++ 3.Windows11_Download_KACE.ps1
@@ -4,7 +4,7 @@ If (Test-Path -Path $Capable) {
     #Download .iso
     Start-Job -Name WebReq -ScriptBlock { 
         $ProgressPreference = 'SilentlyContinue'
-        $URL = "https://software.download.prss.microsoft.com/dbazure/Win11_22H2_English_x64v1.iso?t=390278cd-bf50-41a5-86c5-3f4b6e29521d&e=1684931139&h=17591f78f7c5bbc3509661b020c5427b80716e7884f2ad40b4504672e0de0a63"
+        $URL = "https://software.download.prss.microsoft.com/dbazure/Win11_22H2_English_x64v2.iso?t=3a7d78ff-f65d-4ea7-9445-c04eb30cf280&e=1686267902&h=c82a34d97e537a85c2ef8d92117a35c2896041ddf1b697a2a1fafec996242807"
         $Path = "C:\Install\Windows11Upgrade.iso"
         Invoke-WebRequest -Uri $URL -OutFile $Path 
     }
